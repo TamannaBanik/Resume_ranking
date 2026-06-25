62a40a41eeca6de2864a9732eeabc6241694a468
libmid <83584402+libmid@users.noreply.github.com>
2026-06-25 12:21:34 +0530

    add: semantic vector search

---
+++ src/vector_search.py
@@ -0,0 +1,24 @@
+import lancedb
+
+
+class VectorSearch:
+    def __init__(self, candidates, db_path):
+        self.target_candidate_ids = [
+            candidate["candidate_id"] for candidate in candidates
+        ]
+        self.db = lancedb.connect(db_path)
+
+    def search(self, query: str, across: str, num_results: int):
+        source_table = self.db.open_table(query)
+        query_vector = source_table.search().to_list()[0]["vector"]
+
+        search_table = self.db.open_table("candidate_resumes")
+        results = (
+            search_table.search(query_vector, vector_column_name=across)
+            .where(f"candidate_id IN ({', '.join(f"'{cid}'" for cid in self.target_candidate_ids)})")
+            .select(["candidate_id", "_distance"])
+            .metric("cosine")
+            .limit(num_results)
+        )
+
+        return results.to_pandas()
