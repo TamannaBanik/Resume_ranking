ea7bb27fc4c2e151368a8c701b781fcdca02501d
libmid <83584402+libmid@users.noreply.github.com>
2026-07-01 19:32:44 +0530

    modify: add json support

--- src/reader.py
+++ src/reader.py
@@ -1,15 +1,20 @@
+import json
 from collections.abc import Iterator
 from typing import Any, Optional
-import json
 
 
 class JsonlReader:
     def __init__(self, filepath: str):
         self.filepath: str = filepath
+        self.isjsonl: bool = filepath.endswith(
+            ".jsonl"
+        )  # json otherwise, we dont support any other types
         self.file: Optional[Any] = None
 
     def __enter__(self) -> "JsonlReader":
         self.file = open(self.filepath, "r")
+        if not self.isjsonl:
+            self.candidates = json.load(self.file)
         return self
 
     def __exit__(
@@ -22,7 +27,10 @@ class JsonlReader:
             self.file.close()
 
     def __iter__(self) -> "JsonlReader":
-        return self
+        if self.isjsonl:
+            return self
+        else:
+            return iter(self.candidates)
 
     def __next__(self) -> dict[str, Any]:
         line = self.file.readline()
